6022280a607af4766dca75da72d5d95f1b8ab6aa
wern0531 <wern780531@hotmail.com>
2023-03-01 22:39:40 +0800

    deploy

--- assets/AboutView-0d3f084b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t}from"./index-0a76a445.js";const s={};function a(c,n,r,p,_,i){return o(),t("h1",null,"This is an about page")}const f=e(s,[["render",a]]);export{f as default};
--- assets/HomeView-3d2dceaa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t}from"./index-0a76a445.js";const c={components:{}};function n(s,r,a,p,_,f){return o(),t("div",null,"這是首頁")}const l=e(c,[["render",n]]);export{l as default};
